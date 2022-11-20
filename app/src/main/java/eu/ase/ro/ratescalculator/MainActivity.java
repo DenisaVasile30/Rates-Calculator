ffb4f169407579a5fb6f8e5e235ea5b79f1980ff
Denisa Vasile <101734145+DenisaVasile30@users.noreply.github.com>
2022-11-20 20:45:00 +0200

    v0

--- app/src/main/java/eu/ase/ro/ratescalculator/MainActivity.java
+++ app/src/main/java/eu/ase/ro/ratescalculator/MainActivity.java
@@ -1,8 +1,14 @@
 package eu.ase.ro.ratescalculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavigationBarView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
+
+    private NavigationBarView.OnItemSelectedListener navListener =
+            new NavigationBarView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_credits:
+                            selectedFragment = new CreditsFragment();
+                            break;
+                        case R.id.nav_deposits:
+                            selectedFragment = new DepositsFragment();
+                            break;
+                        case R.id.nav_more_options:
+                            selectedFragment = new MoreOptionsFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
