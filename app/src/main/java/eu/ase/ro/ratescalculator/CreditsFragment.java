f4058fc1297a6879af6f5f607fa4c464bd0542fe
Denisa Vasile <101734145+DenisaVasile30@users.noreply.github.com>
2022-11-20 23:04:29 +0200

    added loan type

--- app/src/main/java/eu/ase/ro/ratescalculator/CreditsFragment.java
+++ app/src/main/java/eu/ase/ro/ratescalculator/CreditsFragment.java
@@ -4,16 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Objects;
+
 public class CreditsFragment extends Fragment {
+
+    private RadioGroup rgCreditType;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        initComponents();
         return inflater.inflate(R.layout.fragment_credits, container, false);
+    }
+
+    private void initComponents() {
+        rgCreditType = requireView().findViewById(R.id.rg_credits_type);
+
     }
 }
