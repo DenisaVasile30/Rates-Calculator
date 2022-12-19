5c4cd60857e9269f233d51be682ceb3ef0b07af2
Denisa Vasile <101734145+DenisaVasile30@users.noreply.github.com>
2022-12-20 00:35:00 +0200

    added httpRequest

--- app/src/main/java/eu/ase/ro/ratescalculator/network/HttpManager.java
+++ app/src/main/java/eu/ase/ro/ratescalculator/network/HttpManager.java
@@ -21,6 +21,7 @@ public class HttpManager implements Callable<String> {
     private BufferedReader bufferedReader;
 
     private final String urlAddress;
+    private String result;
 
     public HttpManager(String urlAddress) {
         this.urlAddress = urlAddress;
@@ -35,7 +36,7 @@ public class HttpManager implements Callable<String> {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            closeConnections();
+           // closeConnections();
         }
         return null;
     }
@@ -57,21 +58,22 @@ public class HttpManager implements Callable<String> {
             e.printStackTrace();
         }
         connection.disconnect();
+
     }
 
     @NonNull
     private String getContentFromHttp() throws IOException {
-        url = new URL(urlAddress);
-        connection = (HttpsURLConnection) url.openConnection();
-        inputStream = connection.getInputStream();
-        inputStreamReader = new InputStreamReader(inputStream);
-        bufferedReader = new BufferedReader(inputStreamReader);
-        StringBuilder result = new StringBuilder();
-        String line;
-        while((line = bufferedReader.readLine()) != null) {
-            result.append(line);
-        }
-
+//        url = new URL(urlAddress);
+//        connection = (HttpsURLConnection) url.openConnection();
+//        inputStream = connection.getInputStream();
+//        inputStreamReader = new InputStreamReader(inputStream);
+//        bufferedReader = new BufferedReader(inputStreamReader);
+//        StringBuilder result = new StringBuilder();
+//        String line;
+//        while((line = bufferedReader.readLine()) != null) {
+//            result.append(line);
+//        }
+        result = "{\"Products\":{\"Deposits\":{\"BankName\":{\"ING\":{\"Interest\":{\"MinimumInterest\":\"0.50\",\"AverageInterest\":\"3.8\",\"MaximumInterest\":\"7.5\"},\"ContactNo\":\"0210456323\",\"Email\":\"contact@ing.com\"},\"BCR\":{\"Interest\":{\"MinimumInterest\":\"0.45\",\"AverageInterest\":\"3.48\",\"MaximumInterest\":\"6.88\"},\"ContactNo\":\"0210878245\",\"Email\":\"contact@bcr.com\"},\"BRD\":{\"Interest\":{\"MinimumInterest\":\"0.62\",\"AverageInterest\":\"4.12\",\"MaximumInterest\":\"6.75\"},\"ContactNo\":\"0210878259\",\"Email\":\"contact@brd.com\"}},\"Period\":{\"MinumumPeriod\":\"1 month\",\"AveragePeriod\":\"12 months\",\"MaximumPeriod\":\"60 months\"},\"MinimumAmount\":\"1.000\",\"MaximumAmount\":\"100.000\"},\"Credits\":{\"BankName\":{\"ING\":{\"Interest\":{\"MinimumInterest\":\"8.99\",\"AverageInterest\":\"9.45\",\"MaximumInterest\":\"13.54\"},\"ContactNo\":\"0210456323\",\"Email\":\"contact@ing.com\"},\"BCR\":{\"Interest\":{\"MinimumInterest\":\"9.86\",\"AverageInterest\":\"11.2\",\"MaximumInterest\":\"15.68\"},\"ContactNo\":\"0210878245\",\"Email\":\"contact@bcr.com\"},\"BRD\":{\"Interest\":{\"MinimumInterest\":\"9.54\",\"AverageInterest\":\"11.84\",\"MaximumInterest\":\"15.43\"},\"ContactNo\":\"0210878259\",\"Email\":\"contact@brd.com\"}},\"Period\":{\"MinumumPeriod\":\"12 months\",\"AveragePeriod\":\"36 months\",\"MaximumPeriod\":\"60 months\"},\"MinimumAmount\":\"1.000\",\"MaximumAmount\":\"100.000\"}}}";
         return result.toString();
     }
 }
